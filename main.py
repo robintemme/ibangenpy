13fc70570abfc3d2d074de9448c765baf949b930
zwerch <zwerch1337@googlemail.com>
2013-12-26 22:42:43 +0100

    added main program

---
+++ main.py
@@ -0,0 +1,13 @@
+__author__ = "n3rdkeller.de"
+__license__ = "GPL"
+__version__ = "0.1"
+__email__ = "github@n3rdkeller.de"
+__status__ = "Development"
+
+def main():
+    while True:
+        print("Welcome to our IBAN generator.")
+        input()
+
+if __name__ == '__main__':
+    main()
