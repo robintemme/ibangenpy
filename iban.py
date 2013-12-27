7c6711d51c451ce14bfc5d5d4dd411b3c0248f9f
zwerch <zwerch1337@googlemail.com>
2013-12-27 09:30:19 +0100

    added country-codes

--- iban.py
+++ iban.py
@@ -4,6 +4,12 @@ __version__ = "0.1"
 __email__ = "github@n3rdkeller.de"
 __status__ = "Development"
 
+
+# global variables
+COUNTRIES = ("AD", "AT", "BE", "CH", "CY", "CZ", "DE", "DK", "EE", "ES", \
+             "FI", "FR", "GB", "GI", "GR", "HU", "IE", "IS", "IT", "LT", \
+             "LU", "LV", "NL", "NO", "PL", "SE", "SI", "SK")
+
 ##############
 # class IBAN #
 ##############
