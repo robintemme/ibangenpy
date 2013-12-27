124177bde785fe3497262c4b44995312d4e441bf
zwerch <zwerch1337@googlemail.com>
2013-12-27 07:16:09 +0100

    added iban-class

---
+++ iban.py
@@ -0,0 +1,14 @@
+__author__ = "n3rdkeller.de"
+__license__ = "GPL"
+__version__ = "0.1"
+__email__ = "github@n3rdkeller.de"
+__status__ = "Development"
+
+##############
+# class IBAN #
+##############
+class iban(object):
+    '''New instance of iban-class'''
+
+    def __init__(self):
+        pass
