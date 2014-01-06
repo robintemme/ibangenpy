83be2778682efd790f89274b0a2c93399961cf08
zwerch <zwerch1337@googlemail.com>
2014-01-06 04:40:20 +0100

    added checksum to implementation

--- iban.py
+++ iban.py
@@ -30,13 +30,26 @@ class Iban(object):
             # I know there are many things that are equal here to other
             # countries. But I decided to only do DE for now.
             fill_to_ten = 10 - len(self.account_number)
+            
             bban = str(self.routing_number) + \
                     (fill_to_ten * "0") + \
                     str(self.account_number)
-            country_code_ascii = 1314
-            checksum = 0
+
+            # country code chars have to get converted first:
+            country_code_int = 131400 # only works for "DE"
+            # I know this is no implementation
+            # but I'm too lazy now
+      
+            # get the checksum
+            checksum = 98 - int(bban + str(country_code_int)) % 97
+            # not sure if this is working:
+            if (checksum < 0) and (abs(checksum) < 10):
+                checksum = "0" + str(abs(checksum))
+
+            # assemble the iban
             my_iban = self.country_code + str(checksum) + bban
             return my_iban
+        
         else:
             print("\nOther countries than 'DE' not yet working.")
             print("Feel free to contribute on GitHub.\n")
